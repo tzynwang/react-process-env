69b96b674c53b7f67eda13a183278a6f0676cb58
tzyn <tzyn.wang@gmail.com>
2022-04-04 13:15:36 +0800

    [feat] Log process.env in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface EnvObject {
+  key: string
+  value: string | number | boolean | undefined
+  type: string
 }
 
-export default App;
+function App(): React.ReactElement {
+  const [displayEnv, setDisplayEnv] = useState<EnvObject[]>([])
+
+  useEffect(() => {
+    console.log(process.env)
+    setDisplayEnv(
+      Object.entries(process.env).map((env) => ({
+        key: env[0],
+        value: env[1],
+        type: typeof env[1]
+      }))
+    )
+  }, [])
+
+  return (
+    <div>
+      <ul>
+        {displayEnv.map((env) => (
+          <li key={env.key}>
+            <div>
+              {env.key}: <code>{env.value}</code>
+            </div>
+            <div>
+              type: <code className={env.type}>{env.type}</code>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
